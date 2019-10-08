603fa11a104d8abee8c77c04ea74d6094cea2779
mac <mohammed.ali.chherawalla@gmail.com>
2019-10-08 13:26:20 +0530

    index tests for ExampleScreen Container

---
+++ app/containers/ExampleScreen/tests/index.test.js
@@ -0,0 +1,24 @@
+/**
+ *
+ * Tests for ExampleScreen
+ *
+ * @see https://github.com/react-boilerplate/react-boilerplate/tree/master/docs/testing
+ *
+ */
+
+import React from 'react'
+import Enzyme, { shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+// import 'jest-dom/extend-expect' // add some helpful assertions
+
+import ExampleScreen from '../index'
+
+Enzyme.configure({ adapter: new Adapter() })
+
+describe('<ExampleScreen />', () => {
+
+  it('Should render and match the snapshot', () => {
+    const component = shallow(<ExampleScreen />)
+    expect(component).toMatchSnapshot()
+  })
+})

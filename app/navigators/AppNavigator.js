d250aed29f100240b2b85f96247d62ccdefee32b
Jay Poojara <jaypoojara007@gmail.com>
2020-11-16 16:45:24 +0530

    Upgraded the react navigation to 4.x

--- app/navigators/AppNavigator.js
+++ app/navigators/AppNavigator.js
@@ -1,4 +1,5 @@
-import { createAppContainer, createStackNavigator } from 'react-navigation';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 import SplashScreen from '@scenes/SplashScreen/';
 import ExampleScreen from '@scenes/ExampleScreen';
 

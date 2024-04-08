1340a13988bce4788e1e75f63b2047d9df1527ee
HIMANSHU <himanshu@wednesday.is>
2024-04-08 15:16:16 +0530

    test for action.js

--- app/components/atoms/LanguageProvider/tests/action.test.js
+++ app/components/atoms/LanguageProvider/tests/action.test.js
@@ -1,19 +1,15 @@
 import { changeLocale } from '../actions';
-import { CHANGE_LOCALE } from '../constants'; // Import your action creator and action type
+import { CHANGE_LOCALE } from '../constants';
 
 // Test suite for the changeLocale action creator
 describe('changeLocale action creator', () => {
   it('should create an action to change the locale', () => {
-    // Define the expected language locale
-    const languageLocale = 'fr';
-    // Expected action object to be returned
+    const languageLocale = 'en';
     const expectedAction = {
       type: CHANGE_LOCALE,
       locale: languageLocale
     };
-    // Call the changeLocale action creator with a languageLocale argument
     const action = changeLocale(languageLocale);
-    // Assert that the action creator returns the correct action
     expect(action).toEqual(expectedAction);
   });
 });

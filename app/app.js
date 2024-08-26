2e90e485c22d4ebadffdb69f1cb06a86d8a6795a
Shamoil <apple@Admins-MacBook-Pro-2.local>
2024-08-26 19:35:19 +0530

    Language Provider and redux store

--- app/app.js
+++ app/app.js
@@ -3,14 +3,22 @@ import { I18nextProvider } from 'react-i18next';
 import LanguageProvider from '@atoms/LanguageProvider';
 import RootScreen from '@scenes/RootScreen';
 import i18n from '@app/i18n';
-import 'react-native-gesture-handler';
+import createStore from '@app/rootReducer';
 
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/lib/integration/react';
+import 'react-native-gesture-handler';
+const { store, persistor } = createStore();
 const App = () => (
-  <I18nextProvider i18n={i18n}>
-    <LanguageProvider>
-      <RootScreen />
-    </LanguageProvider>
-  </I18nextProvider>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <I18nextProvider i18n={i18n}>
+        <LanguageProvider>
+          <RootScreen />
+        </LanguageProvider>
+      </I18nextProvider>
+    </PersistGate>
+  </Provider>
 );
 
 export default App;

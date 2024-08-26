ec00ffb4ba66acf58ec84134a2c63650e80d6de4
rohankeskarwednesday <rohan.keskar@wednesday.is>
2024-08-26 11:03:43 +0530

    feat: sentry integration

--- App.js
+++ App.js
@@ -4,6 +4,12 @@
 
 import { registerRootComponent } from 'expo';
 import App from '@app/app';
+import * as Sentry from '@sentry/react-native';
+
+Sentry.init({
+  dsn:
+    'https://0fbf25b4bfb443b7ae58aa4baf34460e@o4505374929584128.ingest.us.sentry.io/4505374931812352'
+});
 
 if (!window.Intl) {
   new Promise(resolve => {
